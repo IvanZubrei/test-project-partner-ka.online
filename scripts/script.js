9048e628c43ee63594b34a7adcc9ffebb6ff76cd
Ivan Zubrei <zubreiivan@gmail.com>
2023-09-19 02:32:29 +0300

    feat: added menu items when clicking on the advertising link and the content link

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,14 @@ const menuBtnClose = document.querySelector('.menu-btn-close');
 const navigationLink = document.querySelector('.menu-mobile');
 const sitesPopup = document.querySelector('.sites-popup');
 const sitesPopupOpenClose = document.querySelector('.header__sites-popup');
-const backgroundColorActiv = document.querySelector('.background-color');
+const sitesBackgroundColorActiv = document.querySelector('.sites-background-color');
+const adPopup = document.querySelector('.ad-popup');
+const adPopupOpenClose = document.querySelector('.header__ad-popup');
+const adBackgroundColorActiv = document.querySelector('.ad-background-color');
+const contentPopup = document.querySelector('.content-popup');
+const contentPopupOpenClose = document.querySelector('.header__content-popup');
+const contentBackgroundColorActiv = document.querySelector('.content-background-color');
+
 const toggleMenuMobile = () => {
   menuBtn.classList.toggle('close-menu-btn');
   menuMobile.classList.toggle('menu-open');
@@ -35,7 +42,11 @@ navigationLink.addEventListener('click', () => {
 
 const toggleSitesPopup = () => {
   sitesPopupOpenClose.classList.toggle("popup-open-close"); 
-  backgroundColorActiv.classList.toggle("popup-background-color");   
+  sitesBackgroundColorActiv.classList.toggle("popup-background-color");  
+  adPopupOpenClose.classList.remove("popup-open-close"); 
+  adBackgroundColorActiv.classList.remove("popup-background-color");
+  contentPopupOpenClose.classList.remove("popup-open-close"); 
+  contentBackgroundColorActiv.classList.remove("popup-background-color");
 }
 
 sitesPopup.addEventListener('click', e => {
@@ -51,5 +62,55 @@ document.addEventListener('click', e => {
   
   if (!its_sitesPopupOpenClose && !its_sitesPopup && sitesPopupOpenClose_is_active) {
     toggleSitesPopup();
+  }
+});
+
+const toggleAdPopup = () => {
+  adPopupOpenClose.classList.toggle("popup-open-close"); 
+  adBackgroundColorActiv.classList.toggle("popup-background-color"); 
+  sitesPopupOpenClose.classList.remove("popup-open-close"); 
+  sitesBackgroundColorActiv.classList.remove("popup-background-color"); 
+  contentPopupOpenClose.classList.remove("popup-open-close"); 
+  contentBackgroundColorActiv.classList.remove("popup-background-color");  
+}
+
+adPopup.addEventListener('click', e => {
+  e.stopPropagation();
+  toggleAdPopup();
+});
+
+document.addEventListener('click', e => {
+  let target = e.target;
+  let its_adPopupOpenClose = target == adPopupOpenClose || adPopupOpenClose.contains(target);
+  let its_adPopup = target == adPopup;
+  let adPopupOpenClose_is_active = adPopupOpenClose.classList.contains('popup-open-close');
+  
+  if (!its_adPopupOpenClose && !its_adPopup && adPopupOpenClose_is_active) {
+    toggleAdPopup();
+  }
+});
+
+const toggleContentPopup = () => {
+  contentPopupOpenClose.classList.toggle("popup-open-close"); 
+  contentBackgroundColorActiv.classList.toggle("popup-background-color"); 
+  sitesPopupOpenClose.classList.remove("popup-open-close"); 
+  sitesBackgroundColorActiv.classList.remove("popup-background-color"); 
+  adPopupOpenClose.classList.remove("popup-open-close"); 
+  adBackgroundColorActiv.classList.remove("popup-background-color");
+}
+
+contentPopup.addEventListener('click', e => {
+  e.stopPropagation();
+  toggleContentPopup();
+});
+
+document.addEventListener('click', e => {
+  let target = e.target;
+  let its_contentPopupOpenClose = target == contentPopupOpenClose || contentPopupOpenClose.contains(target);
+  let its_contentPopup = target == contentPopup;
+  let contentPopupOpenClose_is_active = contentPopupOpenClose.classList.contains('popup-open-close');
+  
+  if (!its_contentPopupOpenClose && !its_contentPopup && contentPopupOpenClose_is_active) {
+    toggleContentPopup();
   }
 });
